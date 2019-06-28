a7a45cb83cce9bf4e8f483475c3f513786c1bbd9
Csala <csala@lancaster.ac.uk>
2019-06-28 20:48:13 +0100

    rearranges archives

---
+++ ro/archive2/sankey/food.js
@@ -0,0 +1,239 @@
+/*This software is released under the MIT License
+
+MIT License 2014 Denes Csala http://www.csaladen.es
+
+The following software uses the javascript frameworks below,
+all of which are distributed under the MIT or GNU/GPL license:
+D3.js http://d3js.org/  data-oriented javascript framework. 
+	- Sankey plugin http://bost.ocks.org/mike/sankey/ for D3.js (modified) by Mike Bostock, 
+	  which is based on the initial version http://tamc.github.io/Sankey/ by Thomas Counsell. 
+	  I have incorporated the ability to render Sankey cycles, as pioneered by https://github.com/cfergus
+	- Dragdealer.js href="http://skidding.github.io/dragdealer/ by Ovidiu Chereches
+*/
+
+//<!--SANKEY DIAGRAM-->
+
+var padding = 10;
+var lowopacity = 0.3;
+var highopacity = 0.7;
+var format2Number = d3.format(",.2f"),
+    formatNumber = d3.format(",.0f"),
+    format = function(a) {
+        return formatNumber(a)
+    },
+    format2 = function(a) {
+        return format2Number(a)
+    },
+    color = d3.scale.category20();
+d3.select("#chart").style("width", document.getElementById("chart").offsetWidth)
+var margin = {
+        top: 40,
+        right: 40,
+        bottom: 80,
+        left: 40
+    },
+    width = document.getElementById("chart").offsetWidth - margin.left - margin.right,
+    height = document.getElementById("chart").offsetHeight - margin.bottom - margin.top;
+var svg = d3.select("#chart").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var sankey = d3.sankey().nodeWidth(30).nodePadding(padding).size([width, height]);
+var path = sankey.reversibleLink();
+
+d3.json("../json/fusions.json", function(error, rawdata) {
+
+    data = rawdata.part
+
+    // console.log(rawdata, data)
+
+    pcolors = {
+        "PRM": "#0d5ca4",
+        "UDMR": "green",
+        "PDL": "darkOrange",
+        "PSD": "#dc2c24",
+        "USR": "#04abe4",
+        "PAC": "grey",
+        "FSN": "grey",
+        "PNL": "#f4d42c",
+        "PD": "darkOrange",
+        "PUNR": "grey",
+        "UNPR": "#a8190f",
+        "PDSR": "#dc2c24",
+        "PNTCD": "#04843c",
+        "FC": "#1d7aad",
+        "PP-DD": "#9966cc",
+        "PAR": "grey",
+        "PL'93": "grey",
+        "MER": "grey",
+        "PMP": "#0484cc",
+        "PC": "#96c2f2",
+        "FDSN": "grey",
+        "ALDE": "#046cab",
+        "PUR-SL": "grey",
+        "PSDR": "#dc2c24",
+        "AUR": "grey",
+        "PER": "grey",
+        "PNL-CD": "grey",
+        "PSM": "grey",
+        "FER": "grey",
+        "PDAR": "grey",
+        "PLS": "grey",
+        "PTLDR": "grey",
+        "PRNR": "grey",
+        "GDC": "grey",
+        "PDM": "grey",
+        "PNL-AT": "grey",
+        "ULB": "grey",
+        "Independent": "#444",
+        "Minorități": "#999",
+        "RMDSZ": "green"
+    }
+
+    var links = [];
+    var nodes = new Set();
+    var nodes2 = [];
+    var noded = [];
+    var min_t = 10;
+    Object.keys(data).forEach(function(d) {
+        Object.keys(data[d]).forEach(function(e) {
+            var target = d.slice(0, d.search(' - '))
+            var source = e.slice(0, e.search(' - ')) + ' '
+            if (data[d][e] > min_t) {
+                nodes.add(source)
+                nodes.add(target)
+            }
+        })
+    })
+
+    nodes = Array.from(nodes);
+
+    for (i = 0; i < nodes.length; i++) {
+        d = nodes[i];
+        c = d.slice(d.search(' ') + 1).trim()
+        nodes2.push({
+            'name': d,
+            "fill": pcolors[c]
+        })
+        noded[d] = i
+    }
+
+    Object.keys(data).forEach(function(d) {
+        Object.keys(data[d]).forEach(function(e) {
+            var target = d.slice(0, d.search(' - '))
+            var source = e.slice(0, e.search(' - ')) + ' '
+            if (data[d][e] > min_t) {
+                links.push({
+                    'source': noded[source],
+                    'target': noded[target],
+                    'value': data[d][e]
+                })
+            }
+        })
+    })
+
+    d = {
+        'nodes': nodes2,
+        'links': links
+    }
+
+    // console.log(d)
+
+    svg.selectAll("g").remove();
+    sankey = d3.sankey().nodeWidth(30).nodePadding(padding).size([width, height]);
+    sankey.nodes(d.nodes).links(d.links).layout(500);
+
+    var g = svg.append("g") //link
+        .selectAll(".link").data(d.links).enter().append("g").attr("class", "link").sort(function(j, i) {
+            return i.dy - j.dy
+        });
+    var h = g.append("path") //path0
+        .attr("d", path(0));
+    var f = g.append("path") //path1
+        .attr("d", path(1));
+    var e = g.append("path") //path2
+        .attr("d", path(2));
+    g.attr("fill", function(i) {
+            if (i.source.fill) return i.source.fill;
+            else return i.source.color = color(i.source.name.replace(/ .*/, ""))
+        }).attr("opacity", lowopacity).on("mouseover", function(d) {
+            d3.select(this).style('opacity', highopacity);
+        }).on("mouseout", function(d) {
+            d3.select(this).style('opacity', lowopacity);
+        }).append("title") //link
+        .text(function(i) {
+            return i.source.name + " -> " + i.target.name + "\n" + format2(i.value)
+        });
+    var c = svg.append("g") //node
+        .selectAll(".node").data(d.nodes).enter().append("g").attr("class", "node").attr("transform", function(i) {
+            return "translate(" + i.x + "," + i.y + ")"
+        }).call(d3.behavior.drag().origin(function(i) {
+            return i
+        }).on("dragstart", function() {
+            this.parentNode.appendChild(this)
+        }).on("drag", b));
+    c.append("rect") //node
+        .attr("height", function(i) {
+            return i.dy
+        }).attr("width", sankey.nodeWidth()).style("fill", function(i) {
+            if (i.fill) return i.color = i.fill;
+            else return i.color = color(i.name.replace(/ .*/, ""))
+        }).style("stroke", function(i) {
+            return d3.rgb(i.color).darker(2)
+        }).on("mouseover", function(d) {
+            svg.selectAll(".link").filter(function(l) {
+                return l.source == d || l.target == d;
+            }).transition().style('opacity', highopacity);
+        }).on("mouseout", function(d) {
+            svg.selectAll(".link").filter(function(l) {
+                return l.source == d || l.target == d;
+            }).transition().style('opacity', lowopacity);
+        }).on("dblclick", function(d) {
+            svg.selectAll(".link").filter(function(l) {
+                return l.target == d;
+            }).attr("display", function() {
+                if (d3.select(this).attr("display") == "none") return "inline"
+                else return "none"
+            });
+        }).append("title").text(function(i) {
+            return i.name + "\n" + format2(i.value)
+
+        });
+    c.append("text") //node
+        .attr("x", -6).attr("y", function(i) {
+            return i.dy / 2
+        }).attr("dy", ".35em").attr("text-anchor", "end").attr("transform", null)
+        .attr("fill", "#889")
+        .attr("stroke-width", 0)
+        // .attr("stroke-width", 1).attr("stroke", "black")
+        .text(function(i) {
+            return i.name
+        })
+        .attr("fill", function(d) {
+            return d3.rgb(d.color).darker(2)
+        })
+        .filter(function(i) {
+            return i.x < width / 2
+        }).attr("x", 6 + sankey.nodeWidth()).attr("text-anchor", "start")
+
+
+
+    c.append("text") //node
+        .attr("x", function(i) {
+            return -i.dy / 2
+        })
+        .attr("y", function(i) {
+            return i.dx / 2 + 6
+        })
+        .attr("transform", "rotate(270)").attr("text-anchor", "middle").text(function(i) {
+            if (i.dy > 30) {
+                return format(i.value);
+            }
+        }).attr("fill", "white").attr("stroke", "black");
+
+
+    function b(i) { //dragmove
+        d3.select(this).attr("transform", "translate(" + (i.x = Math.max(0, Math.min(width - i.dx, d3.event.x))) + "," + (i.y = Math.max(0, Math.min(height - i.dy, d3.event.y))) + ")")
+        sankey.relayout();
+        f.attr("d", path(1));
+        h.attr("d", path(0));
+        e.attr("d", path(2))
+    };
+})
